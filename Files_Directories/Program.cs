aa37b6fd766cfdaae23bd66f50e7e76162256bb0
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-20 22:14:51 +0300

    Добавьте файлы проекта.

---
+++ Files_Directories/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace Files_Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            string path = @"G:\QALearning";
+            Directory.CreateDirectory(path);
+            
+
+            string filepath = @"G:\QALearning\UserData.txt";
+            File.Create(filepath).Close();
+
+
+
+            User carl  = new User("admin", "carl");
+
+            using (StreamWriter stream = new StreamWriter(filepath))
+            {
+                stream.WriteLine($"Role: {carl.role} \nLogin: {carl.login}");
+            }
+        }
+    }
+
+ 
+    public class User
+    {
+        public string role;
+        public string login;
+
+        public User(string r, string l)
+        {
+            role = r;
+            login = l;
+        }
+
+    }
+    
+
+    
+}
